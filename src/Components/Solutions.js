a69561e160f8f38a0b13bcf9fcc1e9a800d5e6b4
rodormz <rodormz98@gmail.com>
2021-09-28 23:02:22 -0500

    Add modal with form for adding solutions and tech.

--- src/Components/Solutions.js
+++ src/Components/Solutions.js
@@ -1,8 +1,27 @@
-import React from "react";
-import { CDBTable, CDBTableHeader, CDBTableBody, CDBContainer } from "cdbreact";
+import React, {useState} from "react";
+import { CDBTable, CDBTableHeader, CDBTableBody, CDBContainer, CDBBtn, CDBModal } from "cdbreact";
+import SolutionsForm from "./SolutionsForm";
 
 const Solutions = () => {
+    const [state, setState] = useState({
+        modal1: false
+      });
+      const toggle = (nr) => () => {
+        let modalNumber = "modal" + nr;
+        setState({
+          ...state,
+          [modalNumber]: !state[modalNumber],
+        });
+      };
     return (
+        <div>
+        <CDBContainer style={{paddingTop: "1.5em", paddingBottom: "1.5em"}}>
+            <CDBBtn color="primary" onClick={toggle(1)} style={{marginRight: "1em"}}>Agregar soluciones</CDBBtn>
+            <CDBBtn color="danger">Remover soluciones</CDBBtn>
+            </CDBContainer>
+            <CDBModal isOpen={state.modal1} toggle={toggle(1)} centered fade>
+                <SolutionsForm/>
+            </CDBModal>
         <CDBTable striped>
             <CDBTableHeader>
                 <tr>
@@ -21,6 +40,7 @@ const Solutions = () => {
                 </tr>
             </CDBTableBody>
         </CDBTable>
+        </div>
     );
 };
 export default Solutions;
--- src/Components/Technologies.js
+++ src/Components/Technologies.js
@@ -1,8 +1,27 @@
-import React from "react";
-import { CDBTable, CDBTableHeader, CDBTableBody } from "cdbreact";
+import React, {useState} from "react";
+import { CDBTable, CDBTableHeader, CDBTableBody, CDBContainer, CDBBtn, CDBModal } from "cdbreact";
+import TechnologiesForm from "./TechnologiesForm";
 
 const Technologies = () => {
+    const [state, setState] = useState({
+        modal1: false
+      });
+      const toggle = (nr) => () => {
+        let modalNumber = "modal" + nr;
+        setState({
+          ...state,
+          [modalNumber]: !state[modalNumber],
+        });
+      };
     return (
+        <div>
+        <CDBContainer style={{paddingTop: "1.5em", paddingBottom: "1.5em"}}>
+            <CDBBtn color="primary" onClick={toggle(1)} style={{marginRight: "1em"}}>Agregar tecnologías</CDBBtn>
+            <CDBBtn color="danger">Remover tecnologías</CDBBtn>
+            </CDBContainer>
+            <CDBModal isOpen={state.modal1} toggle={toggle(1)} centered fade>
+                <TechnologiesForm/>
+            </CDBModal>
         <CDBTable striped>
             <CDBTableHeader>
                 <tr>
@@ -21,6 +40,7 @@ const Technologies = () => {
                 </tr>
             </CDBTableBody>
         </CDBTable>
+        </div>
     );
 };
 export default Technologies;

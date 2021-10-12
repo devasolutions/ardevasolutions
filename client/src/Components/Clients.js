35bf63126659935defa1f2d98dcd620b69d08fe1
paqotote <73367133+paqotote@users.noreply.github.com>
2021-10-12 17:54:16 -0500

    Se agregaron los archivos de Clients y Developers, ademas se agregaron las routes al grid.
    Agregue Clients.js, ClientsForm.js, Developers.js y DevelopersForm.js ademas agregue las rutas correspondientes a Grid.js.

---
+++ client/src/Components/Clients.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import { CDBTable, CDBTableHeader, CDBTableBody, CDBContainer, CDBBtn, CDBModal } from "cdbreact";
+import ClientsForm from "./ClientsForm";
+
+const Clients = () => {
+    const [state, setState] = useState({
+        modal1: false
+    });
+    const toggle = (nr) => () => {
+        let modalNumber = "modal" + nr;
+        setState({
+            ...state,
+            [modalNumber]: !state[modalNumber],
+        });
+    };
+    return (
+        <div>
+        <CDBContainer style={{paddingTop: "1.5em", paddingBottom: "1.5em"}}>
+            <CDBBtn color="primary" onClick={toggle(1)} style={{marginRight: "1em"}}>Agregar clientes</CDBBtn>
+            <CDBBtn color="danger">Remover clientes</CDBBtn>
+        </CDBContainer>
+        <CDBModal isOpen={state.modal1} toggle={toggle(1)} centered fade>
+            <ClientsForm/>
+        </CDBModal>
+        <CDBTable striped>
+            <CDBTableHeader>
+                <tr>
+                    <th>#</th>
+                    <th>Nombre</th>
+                    <th>Tecnologías</th>
+                </tr>
+            </CDBTableHeader>
+            <CDBTableBody>
+                <tr>
+                    <td>1</td>
+                    <td>Placeholder text</td>
+                    <td>Placeholder text</td>
+                </tr>
+            </CDBTableBody>
+        </CDBTable>
+        </div>
+    );
+};
+export default Clients;
---
+++ client/src/Components/DevelopersForm.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { CDBInput, CDBCard, CDBCardBody, CDBBtn, CDBContainer } from "cdbreact";
+
+const DevelopersForm = () => {
+  return (
+    <CDBContainer>
+            <CDBCard style={{ width: "30rem" }}>
+              <CDBCardBody className="mx-4">
+                <div className="text-center mt-4 mb-4">
+                  <p className="h4"> Agregar un nuevo developer </p>
+                </div>
+                <label htmlFor="defaultContactName" className="text-muted m-0">
+                  Campo 1
+                </label>
+                <CDBInput id="defaultContactName" className="mt-n3" type="text" />
+                <label htmlFor="defaultContactEmail" className="text-muted m-0">
+                  Campo 2
+                </label>
+                <CDBInput id="defaultContactEmail" className="mt-n3" type="email" />
+                <label htmlFor="defaultContactSubject" className="text-muted m-0">
+                  Campo 3
+                </label>
+                <CDBInput id="defaultContactSubject" className="mt-n3" type="text" />
+                <label htmlFor="defaultContactMessage" className="text-muted m-0">
+                  Campo 4
+                </label>
+                <CDBInput id="defaultContactMessage" className="mt-n3" type="textarea" />
+                <CDBBtn
+                  outline
+                  color="secondary"
+                  style={{width:"40%"}}
+                  className="btn-block mt-5 mx-auto" >
+                  Guardar
+                </CDBBtn>
+              </CDBCardBody>
+            </CDBCard>
+    </CDBContainer>
+  );
+};
+export default DevelopersForm;

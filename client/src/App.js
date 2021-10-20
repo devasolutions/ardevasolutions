1b4b793fc1c4b88ab31b80e17f5b0031f609289d
Rodolfo <rodormz98@gmail.com>
2021-10-19 21:49:49 -0500

    Fix para navegación no redireccionando correctamente

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import Grid from './Components/Grid';
 import Home from './Components/Home'
 import Login from './Components/Login'
+import Clients from './Components/Clients';
+import Solutions from './Components/Solutions';
+import Developers from './Components/Developers';
+import Technologies from './Components/Technologies';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -12,8 +16,21 @@ function App() {
         <Route path='/login'>
         <Login/>
         </Route>
-        <Route path='/grid'>
+        <Route path='/technologies'>
           <Grid/>
+          <Technologies/>
+        </Route>
+        <Route path='/solutions'>
+          <Grid/>
+          <Solutions/>
+          </Route>
+          <Route path='/developers'>
+          <Grid/>
+          <Developers/>
+          </Route>
+          <Route path='/clients'>
+          <Grid/>
+          <Clients/>
         </Route>
         <Home/>
       </Switch>
